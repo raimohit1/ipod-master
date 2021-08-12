9e799f21d22f9a076d47928347a4382c2cf3fc2b
raimohit1 <88244276+raimohit1@users.noreply.github.com>
2021-08-12 18:46:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## Ipod
+
+This application is clone of ipod .You can use all features of ipod using this application build using reactjs 
+
+
+### Tech used 
+
+Reactjs 
+
+
+
+### Future work 
+
+1 Adding games in game section 
+
+2 Working on music feature 
+
+3 Feature to change theme and background in settings 
+
+
+
+
+
+#### Tu view working demo of application 
+
+https://youtu.be/4rP2ORkyudo
---
+++ src/components/music/artist.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Artist =()=>
+{
+    return (
+        <div style={{height:'207px',width:'300px'}}>
+        <div className='song'>.
+           <div className='artist'></div>
+           <div className='artist-info'>
+               <h3>Ed Sharren</h3>
+               <p>Known For Shape of You</p>
+           </div>
+            </div>
+             </div>
+    )
+}
+
+export default Artist;
